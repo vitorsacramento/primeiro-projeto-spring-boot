1ce1fb6ee68a1268a237557eddd7fafdf0599bc7
Vitor Sacramento <vitorsacramento85@gmail.com>
2022-10-17 21:50:51 -0300

    Mapeameamento relacional da entidade Pessoa

--- src/main/java/com/vitorsacramento/api/model/Pessoa.java
+++ src/main/java/com/vitorsacramento/api/model/Pessoa.java
@@ -1,12 +1,21 @@
 package com.vitorsacramento.api.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Pessoa {
 
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@EqualsAndHashCode.Include
 	private Long id;
 	private String nome;
 	private Integer idade;
