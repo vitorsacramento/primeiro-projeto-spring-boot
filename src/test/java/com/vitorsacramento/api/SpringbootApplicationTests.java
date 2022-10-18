62ba74f3e7bb2e31a1001a60d5a8aff64a46f977
Vitor Sacramento <vitorsacramento85@gmail.com>
2022-10-17 21:33:00 -0300

    Projeto criado

---
+++ src/test/java/com/vitorsacramento/api/SpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorsacramento.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
