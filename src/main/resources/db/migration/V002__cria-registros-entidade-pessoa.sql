6eed220e1f7cfef0c4fd005167a37e9c9a7d9503
Vitor Sacramento <vitorsacramento85@gmail.com>
2022-10-17 22:05:32 -0300

    Insert registros na entidade Pessoa

---
+++ src/main/resources/db/migration/V002__cria-registros-entidade-pessoa.sql
@@ -0,0 +1,4 @@
+INSERT INTO pessoa VALUES(1, 'Peter Parker', 35, 'peter@email.com', '(21) 0000-0000');
+INSERT INTO pessoa VALUES(2, 'Tony Stark', 52, 'tony@email.com', '(21) 0000-0000');
+INSERT INTO pessoa VALUES(3, 'Wade Wilson', 42, 'wade@email.com', '(21) 0000-0000');
+INSERT INTO pessoa VALUES(4, 'Scott Lang', 43, 'scott@email.com', '(21) 0000-0000');
